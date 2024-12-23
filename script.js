c2a8e761b5efbf878c466cf5f0efc616ae5b50f2
ito <nanisiyou1@gmail.com>
2024-12-23 20:34:09 +0900

    運勢決定

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 
 console.log('Hello, world!');
 
+// 運勢リスト
+const fortuneTitles = ["失物", "待人", "学問", "金運", "研究", "推し", "アニメ", "ゲーム", "ガチャ"];
+
+// 待ち人、百万人来る、3人来る、行けたら行く(来ない)
+// ガチャ、当たる百連を引け
+// 研究、終わる、終わらせよ
+
+// MutationObserverを設定
+const observer = new MutationObserver((mutationsList, observer) => {
+	mutationsList.forEach(mutation => {
+			if (mutation.type === 'childList') {
+					console.log('ボタンの内容が変更されました:', mutation);
+			}
+	});
+});
+
 // 要素取得
 const fortuneSlipContainer = document.getElementById('fortune-slip-container');
 const fortuneSlipCell = document.getElementById('cell-template');
@@ -15,12 +31,13 @@ const cellSize = width / 9;
 
 // タイトル設置
 for (let i = 1; i < 10; i++) {
-	const title = fortuneSlipCell.content.cloneNode(true);
-	title.firstElementChild.textContent = `${i}の段`;
-	title.firstElementChild.style.width = `${cellSize}px`;
-	title.firstElementChild.style.wordBreak = "break-all";
-	title.firstElementChild.style.fontSize = "0.9rem";
-	fortuneSlipContainer.append(title);
+	const text = document.createElement('p');
+	text.textContent = `${i}の段\n${fortuneTitles[i - 1]}`;
+	text.classList.add('fortune-slip-cell');
+	text.style.width = `${cellSize}px`;
+	text.style.wordBreak = "break-all";
+	text.style.fontSize = "0.9rem";
+	fortuneSlipContainer.append(text);
 }
 
 // ボタン設置
@@ -40,10 +57,17 @@ for (let i = 1; i < 10; i++) {
 
 		// 下部に式を表示
 		const text = document.createElement('p');
-		text.textContent = `${i} × ${j}`;
+		text.textContent = `${j} × ${i}`;
 		text.style.fontSize = '0.8rem';
 		text.style.color = '#ff2121';
 		button.appendChild(text);
+
+		// オブザーバーをボタンに登録
+		observer.observe(button, { childList: true, subtree: true });
+		// リスナー設定
+		button.addEventListener('click', () => {
+			console.log(i + "," + j +': ボタンがクリックされました:');
+		});
 
 		fortuneSlipContainer.append(cell);
 	}
